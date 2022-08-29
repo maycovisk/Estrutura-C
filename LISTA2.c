9a951a3922080f011123ed00b11a944ca2a32812
Maycovisk <90663036+maycovisk@users.noreply.github.com>
2022-08-29 13:25:56 -0300

    Add files via upload

--- LISTA2.c
+++ LISTA2.c
@@ -31,7 +31,7 @@ return enter();
 }
 
 void ex2(){
-printf("\nEXERCICIO 2 \nProgama que calcule e escreva \na soma: 1/1 + 3/2 + 5/3 + 7/4...+99/50.\n\n");
+printf("\nEXERCICIO 2 \nProgama que calcule e escreva \na soma: 1/1 + 3/2 + 5/3 + 7/4...+ 99/50.\n\n");
 float div, divid, soma;
 
 
