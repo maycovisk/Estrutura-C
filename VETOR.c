5c7b9c4b502a5d004a843504cc089c46c40d59c3
Maycovisk <90663036+maycovisk@users.noreply.github.com>
2022-09-01 22:09:33 -0300

    Add files via upload

---
+++ VETOR.c
@@ -0,0 +1,19 @@
+//VETORES
+
+//TIPO VARIAVEL[QT DE INDICES]
+int vetor[5];
+
+vetor[4] = 18;
+vetor[0] = 7;
+
+
+char nome[60];
+scanf("%s", nome); //%s entra somente uma palavra
+scanf("%[^\n]", nome); //%[^\n] entra até que encontre o \n, com espaço antes do % elimina o buffer
+
+printf("Nome: %s", nome); //saida de todo o nome
+printf("Letra: %c", nome[0]); //saida somente da letra na posicao
+
+
+
+
