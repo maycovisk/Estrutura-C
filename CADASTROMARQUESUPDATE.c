319dc836154d7d588ecc54200d92ad72674aff5a
Maycovisk <90663036+maycovisk@users.noreply.github.com>
2022-09-03 11:16:00 -0300

    Add files via upload

--- CADASTROMARQUESUPDATE.c
+++ CADASTROMARQUESUPDATE.c
@@ -29,9 +29,9 @@ int main(){
         case 0:
             printf("Obrigado por utilizar o SisteMarques!\n");
             break;
+
         case 1:
             printf("===================== NOVO CADASTRO ====================\n");
-            status[contUser] = 1;
             printf("Entre com as informacoes do novo usuario: \n");
             printf("NOME: ");
             scanf(" %[^\n]", nome[contUser]);
@@ -49,48 +49,91 @@ int main(){
 
             system("cls");
             break;
+
         case 2:
 
             for (int i = 0; i < contUser;i++)
             {
-                if(status[i] == 1){
-                    printf("===================== USUARIO %d ======================\n", i + 1);
-                    printf("| NOME: %s\n", nome[i]);
-                    printf("| TELEFONE: (%d) %d-%.4d\n", ddd[i], tel[i]/10000, tel[i]%10000);
-                    printf("| E-MAIL: %s\n", email[i]);
-                    printf("| STATUS: %i\n", status[i]);
-                    printf("======================================================\n\n");
-                }
+                printf("===================== USUARIO %d ======================\n", i + 1);
+                printf("| NOME: %s\n", nome[i]);
+                printf("| TELEFONE: (%d) %d-%.4d\n", ddd[i], tel[i]/10000, tel[i]%10000);
+                printf("| E-MAIL: %s\n", email[i]);
+                printf("======================================================\n\n");
             }
             break;
+
         case 3:
-            printf("===================== BUSCAR CADASTRO ===================\n");
+            printf("===================== PESQUISAR CADASTRO ===================\n");
             printf("Entre com o nome a ser verificado: ");
             scanf(" %[^\n]", nomeAux);
             /**/
                 for (int i = 0; i < contUser; i++)
                 {
-                    if(status[i] == 1){
-                        if(strcmp(nome[i], nomeAux) == 0 ){
-                            printf("===================== USUARIO %d ======================\n", i + 1);
-                            printf("| NOME: %s\n", nome[i]);
-                            printf("| TELEFONE: (%d) %d-%.4d\n", ddd[i], tel[i]/10000, tel[i]%10000);
-                            printf("| E-MAIL: %s\n", email[i]);
-                            printf("======================================================\n\n");
-                            break;
-                        }
-                    }else{printf("Cadastro nao encontrado\n");
+                    if(strcmp(nome[i], nomeAux) == 0 ){
+                        printf("===================== USUARIO %d ======================\n", i + 1);
+                        printf("| NOME: %s\n", nome[i]);
+                        printf("| TELEFONE: (%d) %d-%.4d\n", ddd[i], tel[i]/10000, tel[i]%10000);
+                        printf("| E-MAIL: %s\n", email[i]);
+                        printf("======================================================\n\n");
+                        break;
                     }
                 }
 
 
             break;
+
+        case 4:
+            printf("===================== EDITAR CADASTRO ===================\n");
+            printf("Entre com o nome a ser editado: ");
+            scanf(" %[^\n]", nomeAux);
+                for (int i = 0; i < contUser; i++)
+                {
+                    if(strcmp(nome[i], nomeAux) == 0 ){
+                        printf("NOME: ");
+                        scanf(" %[^\n]", nome[i]);
+
+                        printf("DDD: ");
+                        scanf("%d", &ddd[i]);
+
+                        printf("TELEFONE: ");
+                        scanf("%d", &tel[i]);
+
+                        printf("E-MAIL: ");
+                        scanf(" %[^\n]", &email[i]);
+                        break;
+                    }
+                }
+            printf("Cadastro editado com sucesso...\n");
+
+            break;
+
+        case 5:
+            printf("===================== DELETAR CADASTRO ===================\n");
+            printf("Entre com o nome do cadastro a ser deletado: ");
+            scanf(" %[^\n]", nomeAux);
+                for (int i = 0; i < contUser; i++)
+                {
+                    if(strcmp(nome[i], nomeAux) == 0 ){
+                        contUser--;
+                        strcpy(nome[i], nome[contUser]);
+                        ddd[i] = ddd[contUser];
+                        tel[i] = tel[contUser];
+                        strcpy(email[i], email[contUser]);
+                        contUser++;
+                        break;
+                    }
+                printf("Cadastro deletado com sucesso...\n");
+                }
+            contUser--;
+
+            break;
+
         case 6:
             printf("======================== SOBRE =======================\n");
             printf("| Este software foi desenvolvido para fins didaticos.|\n");
             printf("| AUTOR: Marques Sousa                               |\n");
             printf("| DATA: 01/09/2022                                   |\n");
-            printf("| ATUALIZADO: Maycon de Lima César                   |\n");
+            printf("| ATUALIZADO: Maycon de Lima Cesar                   |\n");
             printf("======================================================\n");
             break;
         default:
