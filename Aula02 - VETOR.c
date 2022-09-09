f84c6a8f0a10a4197e10165cb11815e851fb1629
Maycovisk <90663036+maycovisk@users.noreply.github.com>
2022-09-08 22:27:32 -0300

    Rename VETOR.c to Aula02 - VETOR.c

--- Aula02 - VETOR.c
+++ Aula02 - VETOR.c
@@ -9,7 +9,7 @@ vetor[0] = 7;
 
 char nome[60];
 scanf("%s", nome); //%s entra somente uma palavra
-scanf("%[^\n]", nome); //%[^\n] entra até que encontre o \n, com espaço antes do % elimina o buffer
+scanf("%[^\n]", nome); //%[^\n] entra atÃ© que encontre o \n, com espaÃ§o antes do % elimina o buffer
 
 printf("Nome: %s", nome); //saida de todo o nome
 printf("Letra: %c", nome[0]); //saida somente da letra na posicao
